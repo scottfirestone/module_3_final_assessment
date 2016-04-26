5590100051c4b8ea617de022e2e698ea8db259db
Scott Firestone <scott.firestone.dev@gmail.com>
2016-04-26 11:33:15 -0600

    Pass items request spec

--- spec/request/items_spec.rb
+++ spec/request/items_spec.rb
@@ -2,10 +2,21 @@ require "rails_helper"
 
 RSpec.describe "Items", type: :request do
   it "returns all the items" do
-    get "/api/v1/items"
+    items = 5.times do |i|
+      Item.create(
+        name: "Name #{i}",
+        description: "Description #{i}",
+        image_url: "Image Url #{i}"
+      )
+    end
+
+    get "/api/v1/items.json"
+
+    json = JSON.parse(response.body)
+    expected_keys = ["name", "description", "image_url"]
 
     expect(response.status).to eq(200)
-    expected_keys = [:name, :description, :image_url]
-    binding.pry
+    expect(json["items"].count).to eq(5)
+    expect(json["items"].first.keys).to eq(expected_keys)
   end
 end
