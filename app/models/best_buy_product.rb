59907f21cfc3f7ba84211826342db669dc7e5f5a
Scott Firestone <scott.firestone.dev@gmail.com>
2016-04-26 09:44:41 -0600

    Create BestBuyService for calling api

--- app/models/best_buy_product.rb
+++ app/models/best_buy_product.rb
@@ -1,7 +1,6 @@
 class BestBuyProduct
-
   def initialize
-    
+
   end
 
 
---
+++ spec/services/best_buy_service_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe "Best buy service", type: :request do
+  it "returns a list of products from a search" do
+    
+  end
+end
